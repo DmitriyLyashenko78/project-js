f95b5b7a52b42d63d524c8331af1c39143481be5
Dmitriy Lyashenko <sv-dm@yandex.ru>
2025-05-10 11:09:25 +0300

    lesson2-4

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,11 +7,17 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.ceil(Math.random() * 6)
+let dice2 = Math.ceil(Math.random() * 6)
+let isWinningDouble = false
 
-/*
+
+if ((dice1 === dice2) && (dice1 + dice1 > 3)) {
+    isWinningDouble = true
+} else {
+    isWinningDouble = false
+}
+
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
 if (isWinningDouble) {
@@ -19,4 +25,4 @@ if (isWinningDouble) {
 } else {
   console.log('Не выигрышный дубль.')
 }
-*/
+
