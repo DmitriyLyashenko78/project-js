a622f0c3a191b6c1ec3d1243b8f9ca54a3097468
Dmitriy Lyashenko <sv-dm@yandex.ru>
2025-05-10 10:48:47 +0300

    lesson2-2

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -11,11 +11,11 @@
 const isAdmin = false
 const isVerifiedUser = true
 const hasSpecialPermission = true
-const hasTemporaryPass = true
+const hasTemporaryPass = false
 
 let isAccess
 
-if ((isAdmin === true || isVerifiedUser === true) && (hasSpecialPermission ===true || hasTemporaryPass === true)){
+if ((isAdmin === true || isVerifiedUser === true) && (hasSpecialPermission === true || hasTemporaryPass === true)){
     isAccess = true
 } else {
     isAccess = false
