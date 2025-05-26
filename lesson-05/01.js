d50fc1ef8026c30e549e5124707ba354c9ed87b1
Dmitriy Lyashenko <sv-dm@yandex.ru>
2025-05-26 14:39:07 +0300

    lesson5-1

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -11,9 +11,19 @@
 */
 
 const game = {
-  resources: {
-    gold: 250,
-    lumber: 100,
-  },
-  addResource() {}
+    resources: {
+        gold: 250,
+        lumber: 100,
+    },
+    addResource(resource, amount) {
+               if (resource === 'gold'){
+            game.resources.gold += amount
+        } else if(resource === 'lumber'){
+                   game.resources.lumber = game.resources.lumber + amount
+               } else {
+                   return 'Invalid resource'
+               }
+
+    }
+
 }
