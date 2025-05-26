cfffb7cccee66ed08f31d1984017dabf8ac5973a
Dmitriy Lyashenko <sv-dm@yandex.ru>
2025-05-26 15:46:07 +0300

    lesson5-3

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -1,14 +1,11 @@
 /*
 Напишите функцию `updateGallery`, которая будет обновлять информацию о произведениях искусства в виртуальной галерее. Функция должна принимать три параметра:
-
 - объект галереи
 - название произведения (ключ)
 - новое значение
-
 Если произведение с таким названием уже есть в галерее, его значение должно быть обновлено. Если произведения нет, оно должно быть добавлено в объект галерею.
 
 Пример использования функции:
-
 const gallery = {
   'Mona Lisa': 'Leonardo da Vinci',
   'Starry Night': 'Vincent van Gogh',
@@ -30,9 +27,22 @@ console.log(gallery)
 */
 
 const gallery = {
-  'Mona Lisa': 'Leonardo da Vinci',
-  'Starry Night': 'Vincent van Gogh',
-  'The Scream': 'Edvard Munch',
+    'Mona Lisa': 'Leonardo da Vinci',
+    'Starry Night': 'Vincent van Gogh',
+    'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(gallery, key, newMeaning) {
+    if (key === "Mona Lisa" & newMeaning === gallery["Mona Lisa"]) {
+        gallery["Mona Lisa"] = newMeaning
+    } else if (key === 'Starry Night' & newMeaning === gallery['Starry Night']) {
+        gallery['Starry Night'] = newMeaning
+    } else if (key === 'The Scream' & newMeaning === gallery['The Scream']) {
+        gallery['The Scream'] = newMeaning
+    } else {
+        gallery[key] = newMeaning
+    }
+    return gallery
+}
+
+console.log(updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali'))
